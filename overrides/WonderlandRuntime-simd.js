1b90a8f735833172abb9dc18fa1b69227e2376f0
rafern <rafern@protonmail.com>
2025-01-21 12:27:09 +0000

    Disable UNPACK_FLIP_Y_WEBGL to save another millisecond

--- overrides/WonderlandRuntime-simd.js
+++ overrides/WonderlandRuntime-simd.js
@@ -371,9 +371,9 @@ var instantiateWonderlandRuntime =
                 const w = img.videoWidth || img.width;
                 const h = img.videoHeight || img.height;
                 _glBindTexture(GLctx.TEXTURE_2D_ARRAY, $4);
-                GLctx.pixelStorei(GLctx.UNPACK_FLIP_Y_WEBGL, true);
-                GLctx.texSubImage3D(GLctx.TEXTURE_2D_ARRAY, 0, $0, $1, $2, w, h, 1, GLctx.RGBA, GLctx.UNSIGNED_BYTE, Module._imageTypes[$3] === 4 ? img.getData() : img);
-                GLctx.pixelStorei(GLctx.UNPACK_FLIP_Y_WEBGL, false);
+                // GLctx.pixelStorei(GLctx.UNPACK_FLIP_Y_WEBGL, true);
+                GLctx.texSubImage3D(GLctx.TEXTURE_2D_ARRAY, 0, $0, $1, $2, w, h, 1, GLctx.RGBA8, GLctx.UNSIGNED_BYTE, Module._imageTypes[$3] === 4 ? img.getData() : img);
+                // GLctx.pixelStorei(GLctx.UNPACK_FLIP_Y_WEBGL, false);
                 GLctx.bindTexture(GLctx.TEXTURE_2D_ARRAY, originalTex)
             },
             915959: ($0, $1) => {
--- overrides/WonderlandRuntime-threads.js
+++ overrides/WonderlandRuntime-threads.js
@@ -454,9 +454,9 @@ var instantiateWonderlandRuntime = (() => {
           const w = img.videoWidth || img.width;
           const h = img.videoHeight || img.height;
           _glBindTexture(GLctx.TEXTURE_2D_ARRAY, $4);
-          GLctx.pixelStorei(GLctx.UNPACK_FLIP_Y_WEBGL, true);
+          // GLctx.pixelStorei(GLctx.UNPACK_FLIP_Y_WEBGL, true);
           GLctx.texSubImage3D(GLctx.TEXTURE_2D_ARRAY, 0, $0, $1, $2, w, h, 1, GLctx.RGBA, GLctx.UNSIGNED_BYTE, Module._imageTypes[$3] === 4 ? img.getData() : img);
-          GLctx.pixelStorei(GLctx.UNPACK_FLIP_Y_WEBGL, false);
+          // GLctx.pixelStorei(GLctx.UNPACK_FLIP_Y_WEBGL, false);
           GLctx.bindTexture(GLctx.TEXTURE_2D_ARRAY, originalTex)
         },
         917899: ($0, $1) => {
